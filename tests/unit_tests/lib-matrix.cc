839fe6d0974c976145099344ddb4094d266f5825
Axen Georget <mail@axen.io>
2019-11-03 17:01:25 +0100

    test(lib, matrix): add addition tests

--- tests/unit_tests/lib-matrix.cc
+++ tests/unit_tests/lib-matrix.cc
@@ -28,5 +28,63 @@ namespace tests::unit_tests
         ASSERT_EQ(3, transposed_matrix.at(1, 1));
         ASSERT_EQ(4, transposed_matrix.at(0, 2));
         ASSERT_EQ(5, transposed_matrix.at(1, 2));
+
+        ASSERT_EQ(0, matrix.at(0, 0));
+        ASSERT_EQ(1, matrix.at(0, 1));
+        ASSERT_EQ(2, matrix.at(1, 0));
+        ASSERT_EQ(3, matrix.at(1, 1));
+        ASSERT_EQ(4, matrix.at(2, 0));
+        ASSERT_EQ(5, matrix.at(2, 1));
+    }
+
+    TEST(LibMatrix, addition_in_place)
+    {
+        auto matrix_1 = create_simple_matrix();
+        auto matrix_2 = create_simple_matrix();
+
+        matrix_1 += matrix_2;
+
+        ASSERT_EQ(0, matrix_1.at(0, 0));
+        ASSERT_EQ(2, matrix_1.at(0, 1));
+        ASSERT_EQ(4, matrix_1.at(1, 0));
+        ASSERT_EQ(6, matrix_1.at(1, 1));
+        ASSERT_EQ(8, matrix_1.at(2, 0));
+        ASSERT_EQ(10, matrix_1.at(2, 1));
+
+        ASSERT_EQ(0, matrix_2.at(0, 0));
+        ASSERT_EQ(1, matrix_2.at(0, 1));
+        ASSERT_EQ(2, matrix_2.at(1, 0));
+        ASSERT_EQ(3, matrix_2.at(1, 1));
+        ASSERT_EQ(4, matrix_2.at(2, 0));
+        ASSERT_EQ(5, matrix_2.at(2, 1));
+    }
+
+    TEST(LibMatrix, addition)
+    {
+        auto matrix_1 = create_simple_matrix();
+        auto matrix_2 = create_simple_matrix();
+
+        auto matrix_result = matrix_1 + matrix_2;
+
+        ASSERT_EQ(0, matrix_result.at(0, 0));
+        ASSERT_EQ(2, matrix_result.at(0, 1));
+        ASSERT_EQ(4, matrix_result.at(1, 0));
+        ASSERT_EQ(6, matrix_result.at(1, 1));
+        ASSERT_EQ(8, matrix_result.at(2, 0));
+        ASSERT_EQ(10, matrix_result.at(2, 1));
+
+        ASSERT_EQ(0, matrix_1.at(0, 0));
+        ASSERT_EQ(1, matrix_1.at(0, 1));
+        ASSERT_EQ(2, matrix_1.at(1, 0));
+        ASSERT_EQ(3, matrix_1.at(1, 1));
+        ASSERT_EQ(4, matrix_1.at(2, 0));
+        ASSERT_EQ(5, matrix_1.at(2, 1));
+
+        ASSERT_EQ(0, matrix_2.at(0, 0));
+        ASSERT_EQ(1, matrix_2.at(0, 1));
+        ASSERT_EQ(2, matrix_2.at(1, 0));
+        ASSERT_EQ(3, matrix_2.at(1, 1));
+        ASSERT_EQ(4, matrix_2.at(2, 0));
+        ASSERT_EQ(5, matrix_2.at(2, 1));
     }
 }
