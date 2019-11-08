8f6b49da7377fc14dafb5f4144b724025e232ad5
Axen Georget <mail@axen.io>
2019-11-08 23:24:52 +0100

    feat(lib, neural_network): add empty NeuralNetwork class

---
+++ lib/neural_network/neural-network.cc
@@ -0,0 +1,10 @@
+/**
+ ** @file neural-network/neural-network.cc
+ ** @brief Implementation of neural_network::NeuralNetwork
+ ** @author Axen Georget
+ */
+
+#include "neural-network.hh"
+
+namespace neural_network
+{} // namespace neural_network
---
+++ lib/neural_network/neural-network.hh
@@ -0,0 +1,15 @@
+/**
+ ** @file neural-network/neural-network.hh
+ ** @brief Declaration neural_network::NeuralNetwork
+ ** @author Axen Georget
+ */
+
+namespace neural_network
+{
+    /// Neural Network data structure
+    class NeuralNetwork
+    {
+    public:
+    private:
+    };
+} // namespace neural_network
